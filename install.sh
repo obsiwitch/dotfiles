b5994b5663ee1854f20ac77260b4fa4730f79cfc
Obside <obsiagae@gmail.com>
2015-06-22 05:28:20 +0200

    promptline.vim's config file

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ DOTFILES_ROOT=$(pwd)
 ln -s $DOTFILES_ROOT/gui/.i3 $HOME/.i3
 
 ln -s $DOTFILES_ROOT/shell/.bashrc $HOME/.bashrc
+ln -s $DOTFILES_ROOT/shell/.shell_prompt.sh $HOME/.shell_prompt.sh
 ln -s $DOTFILES_ROOT/shell/.bashrc.d $HOME/.bashrc.d
--- rm.sh
+++ rm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 rm $HOME/.i3
+
 rm $HOME/.bashrc
+rm $HOME/.shell_prompt.sh
 rm $HOME/.bashrc.d
