10ea55aa2f42019d41efb7110014d4a24f658152
Obside <obsiagae@gmail.com>
2015-07-03 22:03:05 +0200

    exit_dialog - use dmenu (old: dzen2)

--- gui/.i3/scripts/exit_dialog.lua
+++ gui/.i3/scripts/exit_dialog.lua
@@ -1,105 +1,54 @@
 #!/usr/bin/lua
 
-local json = require("cjson")
+normal_background_color="#282828"
+normal_foreground_color="#F8F8F2"
+selected_background_color="#d6d6d6"
+selected_foreground_color="#282828"
 
 local conf = {
-    bg_title = "#282828",
-    fg_title = "#D6D6D6",
-    bg = "#D6D6D6",
-    fg = "#282828",
-    width = 200,
-    font = "-*-fixed-*-*-*-*-12-*-*-*-*-*-*-*"
+    normal_bg = "#282828",
+    normal_fg = "#F8F8F2",
+    selected_bg = "#D6D6D6",
+    selected_fg = "#282828"
 }
 
 local actions = {
-    { name = "restart i3 (1)", key="key_1", cmd = "i3-msg restart" },
-    { name = "sleep (2)", key="key_2", cmd = "xflock4:systemctl suspend" },
-    { name = "hibernate (3)", key="key_3", cmd = "xflock4:systemctl hibernate" },
-    { name = "logout (4)", key="key_4", cmd = "i3-msg exit" },
-    { name = "restart (5)", key="key_5", cmd = "systemctl reboot" },
-    { name = "shutdown (6)", key="key_6", cmd = "systemctl poweroff" },
+    ["1. reload"] = "i3-msg restart",
+    ["2. sleep"] = "xflock4;systemctl suspend",
+    ["3. hibernate"] = "xflock4;systemctl hibernate",
+    ["4. logout"] = "i3-msg exit",
+    ["5. restart"] = "systemctl reboot",
+    ["6. shutdown"] = "systemctl poweroff",
 }
 
-function get_current_workspace_rect()
-    local cmd_out = io.popen("i3-msg -t get_workspaces")
-    local jsonWorkspaces = cmd_out:read("*l")
-    local workspaces = json.decode(jsonWorkspaces)
-    
-    for _,ws in pairs(workspaces) do
-        if ws.focused and ws.visible then
-            return ws.rect
-        end
-    end
-end
-
--- place dialog at the center of the current workspace
-function get_dialog_position()
-    local ws_rect = get_current_workspace_rect()
-    
-    return {
-        x = ws_rect.x + ws_rect.width/2 - conf.width/2,
-        y = ws_rect.y + ws_rect.height/2 - conf.width/2,
-    }
-end
-
--- List of events handled by dzen2. Filled with entries from the actions table.
-function get_events()
-    local events = {
-        "onstart=uncollapse,grabkeys", -- uncollapsed menu, grab keys
-        "key_Escape=exit"
-    }
-    
-    for _,action in pairs(actions) do
-        table.insert(events, action.key .. "=exec:" .. action.cmd .. ",exit")
-    end
-    
-    return events
-end
-
-function get_dialog_title()
-    return "^fg(" .. conf.fg_title .. ")^bg(" .. conf.bg_title .. ")" ..
-           "Shutdown options" ..
-           "^fg()^bg()\n"
-end
-
--- Text to display inside the dialog
-function get_dialog_msg()
+function get_menu_choices()
     local action_names = {}
-    for _,action in pairs(actions) do
-        table.insert(action_names, action.name)
+    for name,_ in pairs(actions) do
+        table.insert(action_names, name)
     end
+    table.sort(action_names)
     
     return table.concat(action_names, "\n")
 end
 
--- Command to display the dialog
-function get_dialog_cmd()
-    local events_str = table.concat(get_events(), ";")
-    local pos = get_dialog_position()
-    
+function get_menu_cmd()
     local cmd_args = {
-        "-p", -- persist EOF
-        "-l " .. #actions, -- number of lines in the dialog
-        "-x " .. pos.x,
-        "-y " .. pos.y,
-        "-w " .. conf.width,
-        "-sa c", -- alignment of slave window
-        "-fn '" .. conf.font .. "'", -- font
-        "-bg '" .. conf.bg .. "'",
-        "-fg '" .. conf.fg .. "'",
-        "-e '" .. events_str .. "'"
+        "-nb '" .. conf.normal_bg .. "'",
+        "-nf '" .. conf.normal_fg .. "'",
+        "-sb '" .. conf.selected_bg .. "'",
+        "-sf '" .. conf.selected_fg .. "'"
     }
     local cmd_args_str = table.concat(cmd_args, " ")
     
-    return "dzen2 " .. cmd_args_str
+    return "dmenu " .. cmd_args_str
 end
 
 -- Display the dialog
-function display_dialog()
-    local cmd = "echo '" .. get_dialog_title() .. get_dialog_msg() .. "'" ..
-                "|" .. get_dialog_cmd()
+function display_menu()    
+    local cmd = io.popen("echo '" .. get_menu_choices() .. "'|" .. get_menu_cmd())
+    local choice = cmd:read("*l")
     
-    os.execute(cmd)
+    os.execute(actions[choice])
 end
 
-display_dialog()
+display_menu()
