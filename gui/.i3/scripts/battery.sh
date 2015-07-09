9b328423f64848680e281c958eec7b2672e376f5
Obside <obsiagae@gmail.com>
2015-07-09 12:30:35 +0200

    i3blocks: remove battery block

--- gui/.i3/scripts/battery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-.i3/scripts/battery.perl
-
-# left click, xfce4-power-manager-settings
-case $BLOCK_BUTTON in
-  1) xfce4-power-manager-settings --device-id=/org/freedesktop/UPower/devices/battery_BAT0 ;;
-esac
