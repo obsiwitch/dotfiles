89e44fb4e8454b7d8cfc30f1bc1eeb0144ce0b65
Obside <obsiagae@gmail.com>
2015-08-09 09:06:53 +0200

    system/common/iptables/

--- system/common/install.sh
+++ system/common/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # iptables
-sudo cp $PWD/system/common/etc/network/if-pre-up.d/iptables \
+sudo cp $PWD/system/common/iptables/iptables \
         /etc/network/if-pre-up.d/iptables
-sudo cp $PWD/system/common/etc/iptables.up.rules \
+sudo cp $PWD/system/common/iptables/iptables.up.rules \
         /etc/iptables.up.rules
-sudo cp $PWD/system/common/etc/ip6tables.up.rules \
+sudo cp $PWD/system/common/iptables/ip6tables.up.rules \
         /etc/ip6tables.up.rules
-sudo cp $PWD/system/common/usr/sbin/iptables-clear \
+sudo cp $PWD/system/common/iptables/iptables-clear \
         /usr/sbin/iptables-clear
-sudo cp $PWD/system/common/usr/sbin/iptables-update \
+sudo cp $PWD/system/common/iptables/iptables-update \
         /usr/sbin/iptables-update
