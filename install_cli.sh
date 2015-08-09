6e1a6dc8a30398b11f02fe1421876be5f144db24
Obside <obsiagae@gmail.com>
2015-08-09 08:01:30 +0200

    cli directory

--- install_cli.sh
+++ install_cli.sh
@@ -2,16 +2,16 @@
 
 # Bin
 mkdir -p $HOME/Bin
-ln -s $PWD/bin/crunchix-disable $HOME/Bin/crunchix-disable
-ln -s $PWD/bin/crunchix-enable $HOME/Bin/crunchix-enable
-ln -s $PWD/bin/flactomp3 $HOME/Bin/flactomp3
+ln -s $PWD/cli/bin/crunchix-disable $HOME/Bin/crunchix-disable
+ln -s $PWD/cli/bin/crunchix-enable $HOME/Bin/crunchix-enable
+ln -s $PWD/cli/bin/flactomp3 $HOME/Bin/flactomp3
 
 # Bash
-ln -s $PWD/shell/bashrc $HOME/.bashrc
-ln -s $PWD/shell/shell_prompt.sh $HOME/.shell_prompt.sh
-ln -s $PWD/shell/bashrc.d $HOME/.bashrc.d
-ln -s $PWD/user/inputrc $HOME/.inputrc
+ln -s $PWD/cli/shell/bashrc $HOME/.bashrc
+ln -s $PWD/cli/shell/shell_prompt.sh $HOME/.shell_prompt.sh
+ln -s $PWD/cli/shell/bashrc.d $HOME/.bashrc.d
+ln -s $PWD/cli/shell/inputrc $HOME/.inputrc
 
 # Profile
-ln -s $PWD/user/profile.d $HOME/.profile.d
-ln -s $PWD/user/profile $HOME/.profile
+ln -s $PWD/cli/profile.d $HOME/.profile.d
+ln -s $PWD/cli/profile $HOME/.profile
