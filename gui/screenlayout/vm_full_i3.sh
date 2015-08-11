6c242371464091fa752d2cc7fcf044b52a3aa84c
Obside <obsiagae@gmail.com>
2015-08-11 21:56:43 +0200

    vm screen layouts: source .fehbg

--- gui/screenlayout/vm_full_i3.sh
+++ gui/screenlayout/vm_full_i3.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 xrandr --output Virtual-0 --mode 1680x1050 --pos 0x0 --rotate normal
+~/.fehbg
--- gui/screenlayout/vm_one_quarter_i3.sh
+++ gui/screenlayout/vm_one_quarter_i3.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 xrandr --output Virtual-0 --mode 848x480 --pos 0x0 --rotate normal
+~/.fehbg
