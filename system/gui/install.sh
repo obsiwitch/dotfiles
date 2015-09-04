1925dff6079608d5f801952d96610194756fd78c
Obside <obsiagae@gmail.com>
2015-09-04 19:09:20 +0200

    chromium default flags

--- system/gui/install.sh
+++ system/gui/install.sh
@@ -2,7 +2,11 @@
 
 # Adwaita-i3-tweaks
 sudo git clone --depth 1 https://gitlab.com/Obside/Adwaita-i3-tweaks.git \
-                    /usr/share/themes/Adwaita-i3-tweaks
+    /usr/share/themes/Adwaita-i3-tweaks
+
+# chromium default flags
+sudo cp $PWD/system/gui/chromium-flags \
+        /etc/chromium.d/default-flags
 
 # xflock4
 sudo cp $PWD/system/gui/xflock4 \
--- system/gui/remove.sh
+++ system/gui/remove.sh
@@ -3,5 +3,8 @@
 # Adwaita-i3-tweaks
 sudo rm -rf /usr/share/themes/Adwaita-i3-tweaks
 
+# chromium default flags
+sudo rm /etc/chromium.d/default-flags
+
 # xflock4
 sudo rm /usr/bin/xflock4
