1efd70fdcec163f78fb83604375ea79675475e84
Obside <obsiagae@gmail.com>
2015-12-25 23:26:54 +0100

    nanoha - Synaptics touchpad

--- system/nanoha/install.sh
+++ system/nanoha/install.sh
@@ -13,3 +13,5 @@ sudo cp $PWD/system/nanoha/etc/systemd/logind.conf \
 # X11
 sudo cp $PWD/system/nanoha/etc/X11/xorg.conf.d/20-intel.conf \
         /etc/X11/xorg.conf.d/20-intel.conf
+sudo cp $PWD/system/nanoha/etc/X11/xorg.conf.d/50-synaptics.conf \
+        /etc/X11/xorg.conf.d/50-synaptics.conf
