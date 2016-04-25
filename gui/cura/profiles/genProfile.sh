b3ee66d23010bd6898ea547a137fa10fc69a9fc2
Obside <obsiagae@gmail.com>
2016-04-25 19:40:19 +0200

    curaGenProfile

---
+++ gui/cura/profiles/genProfile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./genProfile.lua d200 fast 3Dmoniak_blue d200_fast.ini
+./genProfile.lua d200 standard 3Dmoniak_blue d200_standard.ini
+./genProfile.lua d200 slow 3Dmoniak_blue d200_slow.ini
