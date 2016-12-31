1f6690842f1944b7c3d85b5f61c948a492845664
Obside <obsidienne@protonmail.com>
2016-12-31 13:45:50 +0100

    system - remove chromium flags

--- system/gui/install.sh
+++ system/gui/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# chromium default flags
-sudo cp $PWD/system/gui/chromium-flags \
-        /etc/chromium.d/default-flags
-
 # xflock4
 sudo cp $PWD/system/gui/xflock4 \
         /usr/bin/xflock4
--- system/gui/remove.sh
+++ system/gui/remove.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# chromium default flags
-sudo rm /etc/chromium.d/default-flags
-
 # xflock4
 sudo rm /usr/bin/xflock4
