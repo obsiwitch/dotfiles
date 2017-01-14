055e44bd5a0aee22c6243e0c24240e44102749a1
Obside <obsidienne@protonmail.com>
2017-01-14 01:42:05 +0100

    cli - rm unused bashrc.d

--- cli/install.sh
+++ cli/install.sh
@@ -7,7 +7,6 @@ ln -s $PWD/cli/bin/* $HOME/Bin/
 # Bash
 ln -s $PWD/cli/shell/bashrc $HOME/.bashrc
 ln -s $PWD/cli/shell/prompt.sh $HOME/.prompt.sh
-ln -s $PWD/cli/shell/bashrc.d $HOME/.bashrc.d
 ln -s $PWD/cli/shell/inputrc $HOME/.inputrc
 
 # Profile
