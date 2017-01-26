ce78369a52c233310373c1429e68948eb98a09ce
Obside <obsidienne@protonmail.com>
2017-01-27 00:01:05 +0100

    nanoha - reorganize obside home
    * /home/obside/encrypted --gocryptfs--> /home/obside/plaintext
    * /home/obside/plaintext & /home/obside/public --unionfs-->
      /home/obside/main
    * obside's $HOME: /home/obside/main

--- system/nanoha/pam.d/pam_gocryptfs.sh
+++ system/nanoha/pam.d/pam_gocryptfs.sh
@@ -4,9 +4,9 @@ case "$PAM_TYPE" in
     "auth")
         read passphrase
 
-        chmod u+w /home/$PAM_USER
-        echo $passphrase | /usr/bin/gocryptfs -allow_other \
-                                /home/$PAM_USER.encrypted \
-                                /home/$PAM_USER
+        chmod u+w /home/$PAM_USER/main
+        echo $passphrase | /usr/bin/gocryptfs \
+                                /home/$PAM_USER/encrypted \
+                                /home/$PAM_USER/plaintext
     ;;
 esac
