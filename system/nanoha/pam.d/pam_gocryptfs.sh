8e992a5cc889afc385de48b02432c4b3e5b521c3
Obside <obsidienne@protonmail.com>
2017-02-13 03:35:00 +0100

    nanoha - obside - modify Private dir hierarchy

--- system/nanoha/pam.d/pam_gocryptfs.sh
+++ system/nanoha/pam.d/pam_gocryptfs.sh
@@ -4,9 +4,9 @@ case "$PAM_TYPE" in
     "auth")
         read passphrase
 
-        chmod u+w /home/$PAM_USER/Private
+        chmod u+w /home/$PAM_USER/Private/plaintext
         echo $passphrase | /usr/bin/gocryptfs \
-                                /home/$PAM_USER/.private \
-                                /home/$PAM_USER/Private
+                                /home/$PAM_USER/Private/encrypted \
+                                /home/$PAM_USER/Private/plaintext
     ;;
 esac
