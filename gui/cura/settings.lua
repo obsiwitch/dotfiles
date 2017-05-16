300c25db76cf7c38f3ec826e7a1ec529c7589c3f
Obsidienne <obsidienne@protonmail.com>
2017-05-16 22:58:40 +0200

    update D200 settings

--- gui/cura/settings.lua
+++ gui/cura/settings.lua
@@ -1,34 +1,33 @@
 local dagoma = {
     name = "D200",
-    
+
     machine_depth = 200,
     machine_height = 200,
-    machine_nozzle_size = 0.4,
-    machine_head_with_fans_polygon = {
-        {-15, 115},
-        {-15, -40},
-        {15, 115},
-        {15, -40}
-    },
     gantry_height = 35,
     machine_width = 200,
-    
+    machine_nozzle_size = 0.4,
+    extruder_head_size_min_x = 15,
+    extruder_head_size_min_y = 40,
+    extruder_head_size_max_x = 15,
+    extruder_head_size_max_y = 115,
+
+
     startGcode = [[
         G90 ; absolute positioning
     	M107 ; fan off
     	M82 ; extruder absolute mode
     	G28 ; home
     	G29 ; Z-Probe
-    	
+
     	G28 Z ; home Z
-    	G92 Z1.42 ; offset
-    	
+    	G92 Z1.40 ; offset
+
     	G1 X100 Y190 Z5 ; move nozzle close to bed
     	M109 S225 ; {print_temperature}
     	G92 E0 ; zero the extruded length
     	G1 E1 ; extrude filament
     	G92 E0 ; zero the extruded length
-    	
+
     	G1 F150 ; set {travel_speed}
     ]],
     endGcode = [[
@@ -42,13 +41,14 @@ local dagoma = {
 }
 
 local general = {
+    profile = low quality,
+
     material_print_temperature = 225,
     material_diameter = 1.75,
-    
-    retraction_speed = 50,
-    retraction_hop_enabled = True,
-    
-    cool_fan_speed_min = 75.0
+    material_retract_at_layer_change = true,
+    rmaterial_etraction_speed = 50,
+
+    travel_z_hop_when_retracted = true
 }
 
 return {
