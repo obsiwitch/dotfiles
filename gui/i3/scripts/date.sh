f5706de21114554e4cb539cf7e352aa628f7042b
Obsidienne <obsiagae@gmail.com>
2018-11-04 11:55:24 +0100

    gui - i3 - fix calendar script

--- gui/i3/scripts/date.sh
+++ gui/i3/scripts/date.sh
@@ -8,17 +8,14 @@
 # and highlight the current day with Pango Text Attribute Markup Language
 # (<https://developer.gnome.org/pango/stable/PangoMarkupFormat.html>).
 calendar_notification() {
-
     today=`date +%e`
     highlight_today="<b><span foreground='#F92672'>$today<\/span><\/b>"
-
-    calendar=`cal | sed "s/$today /$highlight_today /g"`
-
-    notify-send Calendar "$calendar"
+    calendar=`cal | sed "s/$today/$highlight_today/g"`
+    notify-send 'Calendar' "$calendar"
 }
 
 case $BLOCK_BUTTON in
-    1) calendar_notification ;; # left click, display calendar
+    1) calendar_notification ;; # left click: display calendar
 esac
 
 date '+%d/%m/%Y - %H:%M'
