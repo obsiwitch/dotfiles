63214efb411680ce8d4f5b95bf1959626c417439
Obsidienne <obsiagae@gmail.com>
2019-02-08 05:13:19 +0100

    cli - prompt - date + simplify git

--- cli/shell/prompt.sh
+++ cli/shell/prompt.sh
@@ -1,58 +1,43 @@
 __prompt() {
     # palette
-    local black="0"
-    local dark_red="1"
-    local dark_green="2"
-    local dark_yellow="3"
-    local dark_blue="4"
-    local dark_purple="5"
-    local dark_cyan="6"
-    local dark_white="7"
-    local gray="8"
-    local red="9"
-    local green="10"
-    local yellow="11"
-    local blue="12"
-    local purple="13"
-    local cyan="14"
-    local white="15"
+    local black='0'   ; local grey='8'
+    local red1='1'    ; local red2='9'
+    local green1='2'  ; local green2='10'
+    local yellow1='3' ; local yellow2='11'
+    local blue1='4'   ; local blue2='12'
+    local purple1='5' ; local purple2='13'
+    local cyan1='6'   ; local cyan2='14'
+    local white1='7'  ; local white2='15'
 
     # $1: content
     # $2: color
     prompt.block() {
+        test -z "$1" && return 1
         local color="\\033[38;5;$2m"
         local reset="\\033[0m"
-
-        echo "[${color}$1${reset}]"
+        echo -n "[${color}$1${reset}]"
     }
 
     prompt.git() {
-        local is_git_tree=`git rev-parse --git-dir > /dev/null 2>&1`
-
-        if $is_git_tree; then
-            if branch=$( { \
-                git symbolic-ref --quiet HEAD \
-             || git rev-parse --short HEAD; \
-            } 2>/dev/null ); then
-                echo `prompt.block ${branch##*/} $blue`
-            fi
-        fi
+        local branch=$( ( \
+            git symbolic-ref --short HEAD \
+         || git rev-parse --short HEAD \
+        ) 2>/dev/null )
+        prompt.block "$branch" $blue2
     }
 
     prompt.line() {
-        local username=`prompt.block "\u" $dark_red`
-        local hostname=`prompt.block "\h" $dark_red`
-        local directory=`prompt.block "\w" $dark_blue`
-        local git=`prompt.git`
-
-        PS1="┌${username}${hostname}${directory}${git}\n└> "
+        local bdate=$(prompt.block $(date +%H:%M) $purple1)
+        local buser=$(prompt.block '\u' $red1)
+        local bhost=$(prompt.block '\h' $red1)
+        local bpwd=$(prompt.block '\w' $blue1)
+        PS1="┌${bdate}${buser}${bhost}${bpwd}$(prompt.git)\n└> "
     }
 
     prompt.title() {
-        local t_wrap="\033]0;"
-        local end_t_wrap="\007"
-
-        echo -ne "${t_wrap}${USER}@${HOSTNAME}: ${PWD}${end_t_wrap}"
+        local start_title="\033]0;"
+        local end_title="\007"
+        echo -ne "${start_title}${USER}@${HOSTNAME}: ${PWD}${end_title}"
     }
 
     prompt.line
