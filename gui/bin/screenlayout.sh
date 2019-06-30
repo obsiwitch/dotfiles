915beee2d3815156f6be3dcb249d86634b856d53
Obsidienne <obsiagae@gmail.com>
2019-07-01 00:48:24 +0200

    gui - i3 - bg

--- gui/bin/screenlayout.sh
+++ gui/bin/screenlayout.sh
@@ -3,13 +3,11 @@
 layout_laptop() {
     xrandr --output HDMI1 --off \
            --output eDP1  --mode 1920x1080 --pos 0x0 --rotate normal
-    wallpaper
 }
 
 layout_home() {
     xrandr --output HDMI1 --mode 1920x1080 --pos 0x0    --rotate normal \
            --output eDP1  --mode 1920x1080 --pos 1920x0 --rotate normal
-    wallpaper
 }
 
 if   [ "$1" == "laptop" ]; then layout_laptop
