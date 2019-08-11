a4a1b53a67e1ab47ea1d5c339e5f17a004e187a0
Obsidienne <obsiagae@gmail.com>
2019-08-11 14:52:41 +0200

    tests - dotargs

---
+++ tests/dotargs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PATH="$PWD/cli/bin:$PATH"
+source dotfail
+
+shellcheck --exclude='SC2034' 'cli/bin/dotargs'
+
+eval "$(dotargs a -l=orem b -i c --dolor=sit d --amet e)"
+out1='declare -a posargs=([0]="a" [1]="b" [2]="c" [3]="d" [4]="e")'
+out2='declare -a optargs=([0]="-l=orem" [1]="-i" [2]="--dolor=sit" [3]="--amet")'
+out3='declare -A kwargs=([--dolor]="sit" [-l]="orem" [-i]="-i" [--amet]="--amet" )'
+[[ "$(declare -p posargs)" == "$out1" ]] || dotfail
+[[ "$(declare -p optargs)" == "$out2" ]] || dotfail
+[[ "$(declare -p kwargs)" == "$out3" ]] || dotfail
+
+exit 0
