4999f8c0241f444d8bbb97f48a000a0380222ec5
Obsidienne <obsiagae@gmail.com>
2019-08-12 08:14:43 +0200

    gui - bin - cbtxt & cbfiles

--- cli/shell/bashrc.d/alias.sh
+++ cli/shell/bashrc.d/alias.sh
@@ -4,7 +4,6 @@
 
 alias ls='ls --group-directories-first --color=auto --human-readable'
 alias grep='grep --color=auto'
-alias cb='xclip -rmlastnl -selection clipboard'
 alias ip='ip -color'
 
 ## cdc - Change directory to current one. Fix "unreachable" directory problems
