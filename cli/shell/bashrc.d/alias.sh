2f2b6345d6cf831e6ba735a8efc698a31a26b2bc
Obsidienne <obsiagae@gmail.com>
2019-12-18 12:09:48 +0100

    cli - shell - diff output similar to git-diff
    unified context + color

--- cli/shell/bashrc.d/alias.sh
+++ cli/shell/bashrc.d/alias.sh
@@ -5,6 +5,7 @@
 alias ls='ls --group-directories-first --color=auto --human-readable'
 alias grep='grep --color=auto'
 alias ip='ip -color'
+alias diff='diff --unified --color=auto'
 
 ## cdc - Change directory to current one. Fix "unreachable" directory problems
 ## after unmounting and remounting a filesystem.
