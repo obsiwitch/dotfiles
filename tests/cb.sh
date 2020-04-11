9aab8aa228f83d05cb7942c727bec2deb3ec2153
Obsidienne <obsidienne@protonmail.com>
2020-04-11 06:08:43 +0200

    user:bin - cbfiles->cbf ; cbtxt->cbt

--- tests/cb.sh
+++ tests/cb.sh
@@ -6,24 +6,24 @@ DOTFILESP="$(realpath "$(dirname "$0")/..")"
 PATH="$DOTFILESP/user/bin:$PATH"
 source dotfail
 
-test_cbtxt() {
-    shellcheck "$DOTFILESP/user/bin/cbtxt"
+test_cbt() {
+    shellcheck "$DOTFILESP/user/bin/cbt"
 
     local str='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    cbtxt "$str"
+    cbt "$str"
     [[ "$(xclip -selection clipboard -out)" == "$str" ]] || dotfail
 }
 
-test_cbfiles() {
-    shellcheck "$DOTFILESP/user/bin/cbfiles"
+test_cbf() {
+    shellcheck "$DOTFILESP/user/bin/cbf"
 
     local str; str="file://$(realpath "$0")"
-    cbfiles "$0"
+    cbf "$0"
     [[ "$(xclip -selection clipboard -out)" \
         == "$(echo -e "copy\n$str")" ]] || dotfail
 }
 
-test_cbtxt
-test_cbfiles
+test_cbt
+test_cbf
 
 exit 0
