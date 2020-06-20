e213d614cd26fc783c27b6ab351d8a09def11738
Obsidienne <obsidienne@protonmail.com>
2020-06-20 04:37:19 +0200

    tests - dotfail -> xtrace

--- tests/dotrename.sh
+++ tests/dotrename.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-set -o errexit -o nounset
+set -o errexit -o nounset -o xtrace
 
 DOTFILESP="$(realpath "$(dirname "$0")/..")"
 PATH="$DOTFILESP/user/bin:$PATH"
-source dotfail
 
 tmpdir=$(mktemp -d)
 trap 'rm -r "$tmpdir"' EXIT
 
 echo 'a' > "$tmpdir/a"
 dotrename "$tmpdir/a"
-[[ -f "$tmpdir/3f786850e387550fdab836ed7e6dc881de23001b" ]] || dotfail
+[[ -f "$tmpdir/3f786850e387550fdab836ed7e6dc881de23001b" ]]
 
 echo 'b' > "$tmpdir/b.b"
 dotrename "$tmpdir/b.b"
-[[ -f "$tmpdir/89e6c98d92887913cadf06b2adb97f26cde4849b.b" ]] || dotfail
+[[ -f "$tmpdir/89e6c98d92887913cadf06b2adb97f26cde4849b.b" ]]
 
 echo 'c' > "$tmpdir/c.c.c"
 dotrename "$tmpdir/c.c.c"
-[[ -f "$tmpdir/2b66fd261ee5c6cfc8de7fa466bab600bcfe4f69.c.c" ]] || dotfail
+[[ -f "$tmpdir/2b66fd261ee5c6cfc8de7fa466bab600bcfe4f69.c.c" ]]
 
 longstr=$(printf '%*s' 240 | tr ' ' 'd')
 echo 'c' > "$tmpdir/d.$longstr"
-dotrename "$tmpdir/d.$longstr" 2>/dev/null && dotfail
-[[ -f "$tmpdir/d.$longstr" ]] || dotfail
+! dotrename "$tmpdir/d.$longstr" 2>/dev/null
+[[ -f "$tmpdir/d.$longstr" ]]
--- tests/dotreplace.sh
+++ tests/dotreplace.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -o errexit -o nounset
+set -o errexit -o nounset -o xtrace
 
 DOTFILESP="$(realpath "$(dirname "$0")/..")"
 PATH="$DOTFILESP/user/bin:$PATH"
-source dotfail
 
 str="$(dotreplace 'lorem ipsum dolor' 'lorem' 'nya')"
-[[ "$str" == 'nya ipsum dolor' ]] || dotfail
+[[ "$str" == 'nya ipsum dolor' ]]
 str="$(dotreplace '&~"#()[]`_;,!/^$|@+-.*!?' '.*' 'nya')"
-[[ "$str" == '&~"#()[]`_;,!/^$|@+-nya!?' ]] || dotfail
+[[ "$str" == '&~"#()[]`_;,!/^$|@+-nya!?' ]]
