ffda57382d513e38a6dde5cde5ecc99412303a1a
Obsidienne <obsidienne@protonmail.com>
2020-08-02 01:57:36 +0200

    user:shell: fzf completion $PWD or $HOME

--- user/shell/bashrc.d/complete.sh
+++ user/shell/bashrc.d/complete.sh
@@ -6,7 +6,7 @@ _fzf_select() {
     local lineleft=${READLINE_LINE:0:$READLINE_POINT}
     local lineright=${READLINE_LINE:$READLINE_POINT}
     local selected=$(
-        fd . --print0 \
+        fd . "$@" --print0 \
         | fzf --reverse --prompt="> ${lineleft}_${lineright} > " \
               --multi --read0 --print0 \
         | xargs -0 --no-run-if-empty printf '%q '
@@ -15,9 +15,10 @@ _fzf_select() {
     READLINE_LINE="${lineleft}${selected}${lineright}"
     READLINE_POINT=$(( $READLINE_POINT + ${#selected} ))
 }
-bind -x '"\C-f": "_fzf_select"'
+bind -x '"\C-f": _fzf_select "$PWD"'
+bind -x '"\C-g": _fzf_select "$HOME" "$HOME/Documents"'
 
-## Completion fallback for commands handled by bash-completion _longopt and _cd.
-## Restore glob completion. See unmerged pull-request [Handle ambiguous globs
-## gracefully #77](https://github.com/scop/bash-completion/pull/77).
+# Completion fallback for commands handled by bash-completion _longopt and _cd.
+# Restore glob completion. See unmerged pull-request [Handle ambiguous globs
+# gracefully #77](https://github.com/scop/bash-completion/pull/77).
 compopt -o bashdefault ls cd
