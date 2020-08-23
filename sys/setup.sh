5fa0acaf8dff7dfe48e0360d37d0f57b01206347
Obsidienne <obsidienne@protonmail.com>
2020-08-23 20:49:29 +0200

    sys.nanoha -> sys

--- sys/setup.sh
+++ sys/setup.sh
@@ -3,7 +3,7 @@
 set -o errexit -o nounset
 
 DOTFILESP="$(realpath "$(dirname "$0")/..")"
-DOTNANOHAP="$DOTFILESP/sys.nanoha"
+DOTSYSP="$DOTFILESP/sys"
 PATH="$DOTFILESP/user/bin:$PATH"
 
 setup.help() {
@@ -66,16 +66,16 @@ setup.bootloader() {
 
 setup.provision() {
     # kernel modules
-    dotcp "$DOTNANOHAP/kernel/blacklist.conf" '/etc/modprobe.d/'
+    dotcp "$DOTSYSP/kernel/blacklist.conf" '/etc/modprobe.d/'
 
     # X11
-    dotcp "$DOTNANOHAP/xorg/"* '/etc/X11/xorg.conf.d/'
+    dotcp "$DOTSYSP/xorg/"* '/etc/X11/xorg.conf.d/'
 
     # firewall
-    dotcp "$DOTNANOHAP/security/nftables.conf" '/etc/'
+    dotcp "$DOTSYSP/security/nftables.conf" '/etc/'
 
     # sudo
-    dotcp "$DOTNANOHAP/security/sudo_group" '/etc/sudoers.d/group'
+    dotcp "$DOTSYSP/security/sudo_group" '/etc/sudoers.d/group'
     chmod 0440 '/etc/sudoers.d/group'
 }
 
