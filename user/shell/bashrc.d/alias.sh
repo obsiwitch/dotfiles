c70dfecc48298bc43897686a25adee4e44c2cbd0
Obsidienne <obsidienne@protonmail.com>
2020-10-01 23:24:47 +0200

    u:shell:alias: rm cdc

--- user/shell/bashrc.d/alias.sh
+++ user/shell/bashrc.d/alias.sh
@@ -8,7 +8,3 @@ ls() { /bin/ls --group-directories-first --color='auto' --human-readable \
 alias grep='grep --color=auto'
 alias ip='ip -color'
 alias diff='diff --unified --color=auto'
-
-## cdc - Change directory to current one. Fix "unreachable" directory problems
-## after unmounting and remounting a filesystem.
-alias cdc='cd "$PWD"'
