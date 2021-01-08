3ec386173d32d89431dad1e4d47b25f1acd9b31b
Obsidienne <obsidienne@protonmail.com>
2021-01-08 12:30:44 +0100

    user:xfpm: disable display power management
    * disable dpms
    * rm xss-lock
    * i3lock: power off screen

--- user/xfce4/xfpm.sh
+++ user/xfce4/xfpm.sh
@@ -23,13 +23,9 @@ xfconf-set 'lid-action-on-ac' 'uint' '2'
 xfconf-set 'critical-power-action' 'uint' '2'
 
 # Display
+xfconf-set 'dpms-enabled' 'bool' 'false'
 xfconf-set 'blank-on-battery' 'int' '0' # xset s 0
-xfconf-set 'dpms-on-battery-sleep' 'uint' '0' # xset dpms 0 _ _
-xfconf-set 'dpms-on-battery-off' 'uint' '5' # xset dpms _ _ 300
-
 xfconf-set 'blank-on-ac' 'int' '0' # xset s 0
-xfconf-set 'dpms-on-ac-sleep' 'uint' '0' # xset dpms 0 _ _
-xfconf-set 'dpms-on-ac-off' 'uint' '5' # xset dpms _ _ 300
 
 xfconf-set 'brightness-on-battery' 'uint' '9'
 xfconf-set 'brightness-on-ac' 'uint' '9'
