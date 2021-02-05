eb717a03ebfcd360c3820eac60a80516360350ca
Obsidienne <obsidienne@protonmail.com>
2021-02-05 13:18:21 +0100

    sys:setup.sh: add linux-lts

--- sys/setup.sh
+++ sys/setup.sh
@@ -55,9 +55,9 @@ setup.sys.init() {
     swapon '/mnt/swapfile'
 
     # packages
-    pacstrap -i '/mnt' base base-devel linux linux-firmware intel-ucode \
-        amd-ucode grub efibootmgr archiso pacman-contrib networkmanager \
-        nftables
+    pacstrap -i '/mnt' base base-devel linux-lts linux linux-firmware \
+        intel-ucode amd-ucode grub efibootmgr archiso pacman-contrib \
+        networkmanager nftables
 
     # fstab
     genfstab -U '/mnt' > '/mnt/etc/fstab'
