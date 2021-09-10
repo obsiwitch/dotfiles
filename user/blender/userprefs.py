83322e3c2a89fcb89b2d791e9670ce3adbc67377
Obsidienne <obsidienne@protonmail.com>
2021-09-10 02:54:00 +0200

    user:blender: enable looptools

--- user/blender/userprefs.py
+++ user/blender/userprefs.py
@@ -2,10 +2,10 @@
 
 import bpy
 
-# Set startup file (startup.blend)
+# Save factory startup (startup.blend)
 bpy.ops.wm.save_homefile()
 
-# Set user preferences (userpref.blend)
+# Set general preferences
 bpy.context.preferences.view.show_splash = False
 bpy.context.preferences.view.show_tooltips_python = True
 bpy.context.preferences.view.show_developer_ui = True
@@ -15,17 +15,18 @@ bpy.context.preferences.inputs.use_auto_perspective = False
 bpy.context.preferences.filepaths.save_version = 0
 bpy.context.preferences.filepaths.use_load_ui = False
 
-## UI required (--background flag won't work)
+# Set keybindings (UI required, --background flag won't work)
 bpykc = bpy.context.window_manager.keyconfigs[0]
 bpykc.preferences.use_select_all_toggle = True
 bpykc.preferences.spacebar_action = 'SEARCH'
 
-## Addons
+# Enable addons
 bpy.ops.preferences.addon_enable(module = 'mesh_f2')
+bpy.ops.preferences.addon_enable(module = 'mesh_looptools')
 bpy.ops.preferences.addon_enable(module = 'rigify')
 bpy.ops.preferences.addon_enable(module = 'obsi_script_utils')
 
-## Save
+# Save preferences (userpref.blend)
 bpy.ops.wm.save_userpref()
 
 # Close window
