0b5d1b879150a1f0f14ca8694644f538c4213b03
Obsidienne <obsidienne@protonmail.com>
2021-10-04 05:10:15 +0200

    archiso: remove

--- archiso/releng/profiledef.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2034
-
-source '/usr/share/archiso/configs/releng/profiledef.sh'
-
-iso_name="dotarchlinux"
-iso_label="DOT_ARCH_$(date +%Y%m)"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3' '-b' '1M')
