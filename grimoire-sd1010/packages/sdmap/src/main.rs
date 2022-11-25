aa4219286229da590d70c6bb2345cb5933163e4e
Obsidienne <obsidienne@protonmail.com>
2022-11-25 03:36:58 +0100

    sd1010:sdmap:rs: joy2keys()

--- grimoire-sd1010/packages/sdmap/src/main.rs
+++ grimoire-sd1010/packages/sdmap/src/main.rs
@@ -1,11 +1,13 @@
 use evdev::{uinput, InputEvent, AbsoluteAxisType as Abs, RelativeAxisType as Rel,
-            Key, DeviceState, EventType};
+            Key, DeviceState, EventType, AbsInfo};
 
 fn key2key(evt_in: InputEvent, key: Key) -> InputEvent {
     return InputEvent::new(EventType::KEY, key.0, evt_in.value());
 }
 
-fn hat2rel(cache: &DeviceState, evt_in: InputEvent, rel: Rel, coeff: f32) -> InputEvent {
+fn hat2rel(cache: &DeviceState, evt_in: InputEvent, rel: Rel, coeff: f32)
+-> InputEvent
+{
     let past_absinfo = cache.abs_vals().unwrap()[evt_in.code() as usize];
     let delta = if evt_in.value() == 0 || past_absinfo.value == 0 {
         0.0
@@ -13,6 +15,23 @@ fn hat2rel(cache: &DeviceState, evt_in: InputEvent, rel: Rel, coeff: f32) -> Inp
         (evt_in.value() - past_absinfo.value) as f32 * coeff
     } as i32;
     return InputEvent::new(EventType::RELATIVE, rel.0, delta);
+}
+
+// map the minimum and maximum values of a joystick axis to the `key_min` and
+// `key_max` events.
+fn joy2keys(evt_in: InputEvent, absinfo: AbsInfo, key_min: Key, key_max: Key)
+-> Vec<InputEvent>
+{
+    if evt_in.value().abs() <= absinfo.resolution() {
+        vec!(InputEvent::new(EventType::KEY, key_min.0, 0),
+             InputEvent::new(EventType::KEY, key_max.0, 0))
+    } else if evt_in.value() == absinfo.minimum() {
+        vec!(InputEvent::new(EventType::KEY, key_min.0, 1))
+    } else if evt_in.value() == absinfo.maximum() {
+        vec!(InputEvent::new(EventType::KEY, key_max.0, 1))
+    } else {
+        vec!()
+    }
 }
 
 fn kbd_map(cache: &DeviceState, evt_in: InputEvent) -> Vec<InputEvent> {
@@ -48,6 +67,12 @@ fn kbd_map(cache: &DeviceState, evt_in: InputEvent) -> Vec<InputEvent> {
         vec!(hat2rel(&cache, evt_in, Rel::REL_X, 0.01))
     } else if evt_in.code() == Abs::ABS_HAT1Y.0 {
         vec!(hat2rel(&cache, evt_in, Rel::REL_Y, -0.01))
+    } else if evt_in.code() == Abs::ABS_Y.0 {
+        joy2keys(evt_in, AbsInfo::new(0, -32767, 32767, 0, 0, 6553),
+                 Key::KEY_PAGEUP, Key::KEY_PAGEDOWN)
+    } else if evt_in.code() == Abs::ABS_X.0 {
+        joy2keys(evt_in, AbsInfo::new(0, -32767, 32767, 0, 0, 6553),
+                 Key::KEY_HOME, Key::KEY_END)
     } else {
         vec!()
     }
@@ -58,15 +83,14 @@ fn main() -> std::io::Result<()> {
     let mut dev_in = evdev::Device::open(path_in)?;
     dev_in.grab()?;
 
-    let mut kbd_mode = true;
-
     let mut dev_keyboard = uinput::VirtualDeviceBuilder::new()?
         .name("Steam Deck sdmapd keyboard")
         .with_keys(&evdev::AttributeSet::from_iter([
             Key::BTN_RIGHT, Key::BTN_LEFT, Key::BTN_MIDDLE, Key::KEY_LEFTMETA,
             Key::KEY_UP, Key::KEY_DOWN, Key::KEY_LEFT, Key::KEY_RIGHT,
             Key::KEY_LEFTSHIFT, Key::KEY_LEFTCTRL, Key::KEY_RIGHTALT,
-            Key::KEY_LEFTALT, Key::KEY_TAB, Key::KEY_COMPOSE
+            Key::KEY_LEFTALT, Key::KEY_TAB, Key::KEY_COMPOSE, Key::KEY_PAGEUP,
+            Key::KEY_PAGEDOWN, Key::KEY_HOME, Key::KEY_END,
         ]))?
         .with_relative_axes(&evdev::AttributeSet::from_iter([
             Rel::REL_X, Rel::REL_Y
@@ -79,6 +103,7 @@ fn main() -> std::io::Result<()> {
         .with_keys(dev_in.supported_keys().unwrap())?
         .build()?;
 
+    let mut kbd_mode = true;
 
     loop {
         let cache: DeviceState = dev_in.cached_state().clone();
