b9da6faef8a0fcc0fbaab2deecb9a4a09db3a47b
Obsidienne <obsidienne@protonmail.com>
2023-01-09 01:11:34 +0100

    user: setup_games

---
+++ user/setup_games.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o xtrace
+
+sourcep="$(realpath "${BASH_SOURCE%/*}")"
+dotfilesp="$(realpath "${BASH_SOURCE%/*}/..")"
+PATH="$sourcep/bin:$PATH"
+
+# wine
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\FileOpenAssociations' /v 'Enable' /d 'N' /f
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\X11 Driver' /v 'UseTakeFocus' /d 'N' /f
--- user/setup_gui.sh
+++ user/setup_gui.sh
@@ -46,8 +46,3 @@ if  [[ -v DISPLAY ]]; then
           "$HOME/.config/blender/$blender_version/"
     "$sourcep/blender/userprefs.py"
 fi
-
-# wine
-wine reg add 'HKEY_CURRENT_USER\Software\Wine\FileOpenAssociations' /v 'Enable' /d 'N' /f
-wine reg add 'HKEY_CURRENT_USER\Software\Wine\X11 Driver' /v 'UseTakeFocus' /d 'N' /f
-
