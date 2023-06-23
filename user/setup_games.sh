649071a43a9cfaf61ebc59702e57bfa2ee90bb01
Obsidienne <obsidienne@protonmail.com>
2023-06-23 19:12:59 +0200

    user: rm suway, wine isolate home, wine dxvk

--- user/setup_games.sh
+++ user/setup_games.sh
@@ -9,3 +9,5 @@ PATH="$sourcep/bin:$PATH"
 # wine
 wine reg add 'HKEY_CURRENT_USER\Software\Wine\FileOpenAssociations' /v 'Enable' /d 'N' /f
 wine reg add 'HKEY_CURRENT_USER\Software\Wine\X11 Driver' /v 'UseTakeFocus' /d 'N' /f
+winetricks isolate_home
+winetricks dlls dxvk
