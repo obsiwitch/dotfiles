a74b9286383f510d9c3662770578de362c2a494c
Obsidienne <obsidienne@protonmail.com>
2023-07-03 12:54:46 +0200

    user:games: wine corefonts, retroarch, gamescope, deps

--- user/setup_games.sh
+++ user/setup_games.sh
@@ -2,12 +2,9 @@
 
 set -o errexit -o nounset -o xtrace
 
-sourcep="$(realpath "${BASH_SOURCE%/*}")"
-dotfilesp="$(realpath "${BASH_SOURCE%/*}/..")"
-PATH="$sourcep/bin:$PATH"
-
 # wine
 wine reg add 'HKEY_CURRENT_USER\Software\Wine\FileOpenAssociations' /v 'Enable' /d 'N' /f
 wine reg add 'HKEY_CURRENT_USER\Software\Wine\X11 Driver' /v 'UseTakeFocus' /d 'N' /f
 winetricks isolate_home
 winetricks dlls dxvk
+winetricks dlls corefonts
