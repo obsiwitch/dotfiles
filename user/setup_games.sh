96aa52b98be281de2426f6789e237e0771dac331
Obsidienne <obsidienne@protonmail.com>
2024-05-27 13:35:23 +0200

    user: fix setup_games missing vars

--- user/setup_games.sh
+++ user/setup_games.sh
@@ -2,6 +2,10 @@
 
 set -o errexit -o nounset -o xtrace
 
+sourcep="$(realpath "${BASH_SOURCE%/*}")"
+dotfilesp="$(realpath "${BASH_SOURCE%/*}/..")"
+PATH="$sourcep/bin:$PATH"
+
 # wine
 winetricks settings mimeassoc=off # disable exporting MIME-type file associations to the native desktop
 winetricks settings isolate_home # remove links to home
