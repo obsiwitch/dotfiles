862a5b359ff94608837260f8f84cf4be7d948820
Obsidienne <obsidienne@protonmail.com>
2024-11-03 09:13:34 +0100

    pkgs:sdmap: split sdmap-sticker into lib

--- packages/sdmap/src/bin/sdmap-sticker.rs
+++ packages/sdmap/src/bin/sdmap-sticker.rs
@@ -1,88 +1,41 @@
-use xkbcommon::xkb;
-use evdev::Key;
-use sdmap::VKBD_LAYOUT;
+use sdmap::keysyms_layout;
 
-const EVDEV_OFFSET: u32 = 8;
 const BUTTONS: [char; 6] = ['a', 'b', 'y', 'x', 's', 'd'];
 
-struct Sticker { state: xkb::State }
-impl Sticker {
-    pub fn new() -> Self {
-        let ctx = xkb::Context::new(0);
-        let keymap = xkb::Keymap::new_from_names(&ctx, "", "", "fr", "", None, 0).unwrap();
-        Self { state: xkb::State::new(&keymap) }
-    }
-
-    fn keymods(&mut self, mods: &[Key], enable: bool) {
-        for keymod in mods {
-            self.state.update_key(
-                keymod.0 as u32 + EVDEV_OFFSET,
-                if enable { xkb::KeyDirection::Down }
-                else { xkb::KeyDirection::Up },
-            );
-        }
-    }
-
-    fn keystring(&mut self, evkey: Key, mods: &[Key]) -> String {
-        if evkey == Key::KEY_UNKNOWN {
-            return "".into();
-        }
-
-        self.keymods(mods, true);
-        let mut result = self.state.key_get_utf8(evkey.0 as u32 + EVDEV_OFFSET);
-        if result.is_empty() {
-            let keysym = self.state.key_get_one_sym(evkey.0 as u32 + EVDEV_OFFSET);
-            result = xkb::keysym_get_name(keysym);
-        }
-        self.keymods(mods, false);
-
-        match result.as_str() {
-            "dead_circumflex" => "^",
-            "dead_acute"      => "´",
-            "dead_grave"      => "`",
-            "dead_diaeresis"  => "¨",
-            "dead_belowdot"   => ".",
-            result => result,
-        }.into()
-    }
-
-    pub fn print_html(&mut self) {
-        println!(r#"<!DOCTYPE html><html><body>
-            <style type="text/css">
-            body {{font-family:monospace; font-size:18px;}}
-            table {{border-collapse: collapse; width:378px; height:378px;}}
-            tr {{border:1px solid black;}}
-            td {{text-align:center;}}
-            td.a {{color:LimeGreen;}}
-            td.b {{color:Crimson;}}
-            td.x {{color:DodgerBlue;}}
-            td.y {{color:Orange;}}
-            td.s {{color:MediumPurple;}}
-            td.d {{color:SlateGray; border-right:1px solid black;}}
-            span.l0 {{font-size:30px; font-weight:bold;}}
-            </style>
-            <table>"#);
-        for row in VKBD_LAYOUT {
-            println!("<tr>");
-            for col in row {
-                for (i, key) in col.into_iter().enumerate() {
-                    if i >= BUTTONS.len() { break; }
-                    let btn = BUTTONS[i];
-                    let l0 = self.keystring(key, &[]);
-                    let print_mods = !"abcdefghijklmnopqrstuvwxyz".contains(&l0);
-                    let l1 = if print_mods { self.keystring(key, &[Key::KEY_LEFTSHIFT]) }
-                           else { "".into() };
-                    let l2 = if print_mods { self.keystring(key, &[Key::KEY_RIGHTALT]) }
-                           else { "".into() };
-                    println!("<td class=\"{btn}\">{l1} <span class=\"l0\">{l0}</span> {l2}</td>");
-                }
-            }
-            println!("</tr>");
-        }
-        println!("</table></body></html>");
-    }
-}
-
 fn main() {
-    Sticker::new().print_html();
+    let layout = keysyms_layout();
+
+    println!(r#"<!DOCTYPE html><html><body>
+        <style type="text/css">
+        body {{font-family:monospace; font-size:18px;}}
+        table {{border-collapse: collapse; width:378px; height:378px;}}
+        tr {{border:1px solid black;}}
+        td {{text-align:center;}}
+        td.a {{color:LimeGreen;}}
+        td.b {{color:Crimson;}}
+        td.x {{color:DodgerBlue;}}
+        td.y {{color:Orange;}}
+        td.s {{color:MediumPurple;}}
+        td.d {{color:SlateGray; border-right:1px solid black;}}
+        span.l0 {{font-size:30px; font-weight:bold;}}
+        </style>
+        <table>"#);
+    for row in layout {
+        println!("<tr>");
+        for col in row {
+            for (i, key) in col.into_iter().enumerate() {
+                if i >= BUTTONS.len() { break; }
+                let btn = BUTTONS[i];
+                let l0 = key[0].clone();
+                let print_mods = !"abcdefghijklmnopqrstuvwxyz".contains(&l0);
+                let l1 = if print_mods { key[1].clone() }
+                       else { "".into() };
+                let l2 = if print_mods { key[2].clone() }
+                       else { "".into() };
+                println!("<td class=\"{btn}\">{l1} <span class=\"l0\">{l0}</span> {l2}</td>");
+            }
+        }
+        println!("</tr>");
+    }
+    println!("</table></body></html>");
 }
--- packages/sdmap/src/lib.rs
+++ packages/sdmap/src/lib.rs
@@ -1,4 +1,5 @@
 use evdev::Key;
+use xkbcommon::xkb;
 
 // Virtual keyboard layout. Unused key slots should be mapped to KEY_UNKNOWN.
 pub const VKBD_LAYOUT: [[[Key; 7]; 2]; 4] = [
@@ -11,3 +12,58 @@ pub const VKBD_LAYOUT: [[[Key; 7]; 2]; 4] = [
     [[Key::KEY_Z, Key::KEY_X, Key::KEY_C, Key::KEY_V, Key::KEY_B, Key::KEY_GRAVE, Key::KEY_F7],
      [Key::KEY_N, Key::KEY_M, Key::KEY_COMMA, Key::KEY_DOT, Key::KEY_SLASH, Key::KEY_102ND, Key::KEY_F8]]
 ];
+
+// ---
+
+const EVDEV_OFFSET: u32 = 8;
+
+fn xkb_mods(state: &mut xkb::State, mods: &[Key], enable: bool) {
+    for keymod in mods {
+        state.update_key(
+            keymod.0 as u32 + EVDEV_OFFSET,
+            if enable { xkb::KeyDirection::Down }
+            else { xkb::KeyDirection::Up },
+        );
+    }
+}
+
+fn xkb_keysym(state: &mut xkb::State, evkey: Key, mods: &[Key]) -> String {
+    if evkey == Key::KEY_UNKNOWN {
+        return "".into();
+    }
+
+    xkb_mods(state, mods, true);
+    let mut result = state.key_get_utf8(evkey.0 as u32 + EVDEV_OFFSET);
+    if result.is_empty() {
+        let keysym = state.key_get_one_sym(evkey.0 as u32 + EVDEV_OFFSET);
+        result = xkb::keysym_get_name(keysym);
+    }
+    xkb_mods(state, mods, false);
+
+    match result.as_str() {
+        "dead_circumflex" => "^",
+        "dead_acute"      => "´",
+        "dead_grave"      => "`",
+        "dead_diaeresis"  => "¨",
+        "dead_belowdot"   => ".",
+        result => result,
+    }.into()
+}
+
+pub fn keysyms_layout() -> [[[[String; 3]; 7]; 2]; 4] {
+    let ctx = xkb::Context::new(0);
+    let keymap = xkb::Keymap::new_from_names(&ctx, "", "", "fr", "", None, 0).unwrap();
+    let mut state = xkb::State::new(&keymap);
+
+    let mut result: [[[[String; 3]; 7]; 2]; 4] = Default::default();
+    for (i, row) in VKBD_LAYOUT.into_iter().enumerate() {
+        for (j, col) in row.into_iter().enumerate() {
+            for (k, key) in col.into_iter().enumerate() {
+                result[i][j][k][0] = xkb_keysym(&mut state, key, &[]);
+                result[i][j][k][1] = xkb_keysym(&mut state, key, &[Key::KEY_LEFTSHIFT]);
+                result[i][j][k][2] = xkb_keysym(&mut state, key, &[Key::KEY_RIGHTALT]);
+            }
+        }
+    }
+    result
+}
