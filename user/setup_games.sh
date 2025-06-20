5f95dde389a1ca4ad2876a0305a5a3fd59481d68
Obsidienne <obsidienne@protonmail.com>
2025-06-20 11:00:28 +0200

    sfx1441g: wine -> umu
    use Proton or GE-Proton through umu/lutris

--- user/setup_games.sh
+++ user/setup_games.sh
@@ -6,12 +6,5 @@ sourcep="$(realpath "${BASH_SOURCE%/*}")"
 dotfilesp="$(realpath "${BASH_SOURCE%/*}/..")"
 PATH="$sourcep/bin:$PATH"
 
-# wine
-winetricks settings mimeassoc=off # disable exporting MIME-type file associations to the native desktop
-winetricks settings isolate_home # remove links to home
-winetricks dlls dxvk
-winetricks dlls corefonts
-winetricks settings alldlls=default # remove all DLL overrides
-
 # retroarch
 cp -r "$sourcep/retroarch" "$HOME/.config/"
